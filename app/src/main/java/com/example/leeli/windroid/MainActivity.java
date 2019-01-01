b175b80fe1b881029b3d1fdc6f435ddeb8472d39
leeli <leeli@live.cn>
2019-01-01 20:19:29 +0800

    Lee's Submit

--- app/src/main/java/com/example/leeli/windroid/MainActivity.java
+++ app/src/main/java/com/example/leeli/windroid/MainActivity.java
@@ -1,21 +1,33 @@
 package com.example.leeli.windroid;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Color;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.os.Build;
-import android.view.Window;
-import android.webkit.WebSettings;
+
 import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.util.Base64;
 
-import org.w3c.dom.Text;
+
+import java.io.IOException;
+
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -26,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView Password;
     private CheckBox isRemember;
     private CheckBox isAuto;
+    private LocalBroadcastManager LoginBroad;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,10 +50,53 @@ public class MainActivity extends AppCompatActivity {
         LoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent body = new Intent(MainActivity.this,body.class);
-                startActivity(body);
+                Login();
             }
         });
+        RegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Register();
+            }
+        });
+        IntentFilter filter = new IntentFilter("com.example.leeli.windroid");
+        LoginBroad = LocalBroadcastManager.getInstance(this);
+        LoginBroad.registerReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String Result = intent.getStringExtra(Intent.EXTRA_TEXT);
+                String ResultTemp[] = Result.split(":");
+                if(ResultTemp[0].equals("Login"))
+                {
+                    if(ResultTemp[1].equals("LoginError"))
+                    {
+                        Toast.makeText(MainActivity.this,"登录失败,请检查账号或用户名", Toast.LENGTH_LONG).show();
+                    }
+                    else
+                    {
+                        String ResultArray[] = ResultTemp[1].split("@");
+                        if(ResultArray[0].equals("LoginSuccess"))
+                        {
+                            Toast.makeText(MainActivity.this,"登录成功,正在转跳...", Toast.LENGTH_SHORT).show();
+                            Intent bodyActivity = new Intent(MainActivity.this,body.class);
+                            startActivity(bodyActivity);
+                        }
+                    }
+                }
+                else if(ResultTemp[0].equals("Register"))
+                {
+                    if(ResultTemp[1].equals("RegisterSuccess"))
+                    {
+                        Toast.makeText(MainActivity.this,"注册成功!", Toast.LENGTH_LONG).show();
+                    }
+                    else
+                    {
+                        Log.i("play","123");
+                        Toast.makeText(MainActivity.this,"注册失败!请更换用户名", Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        },filter);
     }
     @Override
     public void onBackPressed()
@@ -88,17 +144,68 @@ public class MainActivity extends AppCompatActivity {
     public void InitLoginBackground() //加载背景动画
     {
         LoginBackground = (WebView)findViewById(R.id.LoginBackground);
-        //LoginBackground.getSettings().setJavaScriptEnabled(true);
-        //LoginBackground.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
-        //LoginBackground.getSettings().setLoadWithOverviewMode(true);
         LoginBackground.loadUrl("file:///android_asset/LoginBackground.html");
     }
     public void InitAlpha()
     {
         View LoginCard = findViewById(R.id.LoginCard);
         LoginCard.setAlpha(0.9f);
-        //View AllButton = findViewById(R.id.AllButton);
-        //AllButton.setAlpha(0.9f);
+    }
+    private void Login()
+    {
+        String UsernameStr = Username.getText().toString();
+        String PasswordStr = Password.getText().toString();
+        String url = "http://192.168.0.102:6888/Login";
+        final OkHttpClient okHttpClient=new OkHttpClient();
+        RequestBody body = new FormBody.Builder()
+                .add("Username", Base64.encodeToString(UsernameStr.getBytes(), Base64.DEFAULT))
+                .add("Password", Base64.encodeToString(PasswordStr.getBytes(), Base64.DEFAULT))
+                .build();
+        final Request request=new Request.Builder().url(url).post(body).build();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Response response=okHttpClient.newCall(request).execute();
+                    if (response.isSuccessful()){
+                        String body="Login:"+response.body().string();
+                        Intent intent = new Intent("com.example.leeli.windroid");
+                        intent.putExtra(Intent.EXTRA_TEXT,body);
+                        LoginBroad.sendBroadcast(intent);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+    private void Register()
+    {
+        String UsernameStr = Username.getText().toString();
+        String PasswordStr = Password.getText().toString();
+        String url = "http://192.168.0.102:6888/Register";
+        final OkHttpClient okHttpClient=new OkHttpClient();
+        RequestBody body = new FormBody.Builder()
+                .add("Username", Base64.encodeToString(UsernameStr.getBytes(), Base64.DEFAULT))
+                .add("Password", Base64.encodeToString(PasswordStr.getBytes(), Base64.DEFAULT))
+                .build();
+        final Request request=new Request.Builder().url(url).post(body).build();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Response response=okHttpClient.newCall(request).execute();
+                    if (response.isSuccessful()){
+                        String body="Register:"+response.body().string();
+                        Intent intent = new Intent("com.example.leeli.windroid");
+                        intent.putExtra(Intent.EXTRA_TEXT,body);
+                        LoginBroad.sendBroadcast(intent);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 }
 
